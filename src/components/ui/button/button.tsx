fc4b62c41cfc173bd83894cf0240cb81c2ced686
grace-luong-goldenowl <grace.luong.gos@gmail.com>
2026-01-19 15:10:19 +0700

    feat: add tailwindcss and shadcn ui

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from './buttonVariants';
+import { type VariantProps } from 'class-variance-authority';
+
+function Button({
+  className,
+  variant = 'default',
+  size = 'default',
+  asChild = false,
+  ...props
+}: React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : 'button';
+
+  return (
+    <Comp
+      data-slot="button"
+      data-variant={variant}
+      data-size={size}
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export default Button;
